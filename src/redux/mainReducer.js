baa12e5f6abfaf912869f908ce718bc09c5ce961
Crowdbotics <team@crowdbotics.com>
2020-05-29 03:49:13 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn254759Reducer from '../features/SignIn254759/redux/reducers'
 import CalendarReducer from '../features/Calendar/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
 
@@ -17,6 +18,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn254759: SignIn254759Reducer,
 Calendar: CalendarReducer,
 EmailAuth: EmailAuthReducer,
 
